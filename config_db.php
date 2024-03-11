effe8e5c75a56f54634f24133d726f64d2c27696
Anthony MEDASSI <amedassi@jbdelasalle.com>
2024-03-11 15:30:38 +0100

    Create config_db.php

---
+++ config_db.php
@@ -0,0 +1,6 @@
+<?php
+define("DBHOST", '192.168.153.10');
+define("DBPORT", '3306');
+define("DBNAME", '202324_qrelec_xxxxxxxx');
+define("DBUSER", 'xxxxxxxx');
+define("DBPASS", 'xxxxxxxxxxxxxxxx');
